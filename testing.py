45b320e698c3220545f08b6cae2350629f9a4495
ANIKETMOHOD <69238843+ANIKETMOHOD@users.noreply.github.com>
2021-11-18 13:39:40 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -51,4 +51,53 @@ class TestCalc(unittest.TestCase):
         	self.assertEqual(calc.fibonacci(5), 5)
         except (ZeroDivisionError,SyntaxError,NameError,IndexError,TypeError,AssertionError,ValueError):
         	print("ERROR => TEST FAIL")
-        	    
+            
+            
+       	    
+    def test_fact(self):
+    	try:
+    		self.assertEqual(calc.fact(1), 1)
+    		self.assertEqual(calc.fact(5), 120) 
+    		self.assertEqual(calc.fact(-2), None) 
+    		self.assertEqual(calc.fact(11111111111111111111111111111111111111111111),None) 
+    		self.assertEqual(calc.fact(0), 1)
+    	except (ZeroDivisionError,SyntaxError,NameError,IndexError,TypeError,AssertionError,ValueError):
+        	print("ERROR => TEST FAIL from FACTORIAL Function")
+    
+    def test_bin_to_dec(self):
+    	try:
+    		self.assertEqual(calc.bin_to_dec(10), 2)
+    		self.assertEqual(calc.bin_to_dec(101010), 42) 
+    		self.assertEqual(calc.bin_to_dec(11111111111111111111), 1048575)
+    	except (ZeroDivisionError,SyntaxError,NameError,IndexError,TypeError,AssertionError,ValueError):
+        	print("ERROR => TEST FAIL from BIN_TO_DEC Function")
+        	
+    def test_dec_to_binary(self):
+    	try:
+    		self.assertEqual(calc.dec_to_binary(1), '1')
+    		self.assertEqual(calc.dec_to_binary(0), '0') 
+    		self.assertEqual(calc.dec_to_binary(3), '11')
+    	except (ZeroDivisionError,SyntaxError,NameError,IndexError,TypeError,AssertionError,ValueError):
+        	print("ERROR => TEST FAIL from DEC_TO_BIN Function")
+    
+    def test_hex_to_dec(self):
+    	try:
+    		self.assertEqual(calc.hex_to_dec("A"), 10)
+    		self.assertEqual(calc.hex_to_dec(1), 1)
+    		self.assertEqual(calc.hex_to_dec("ABBBBBBBBBBBBBBBBBBBBBBBBBBBB1000000000000000"), 1028049091997533775815391130713332398302880011684675584)
+    		self.assertEqual(calc.hex_to_dec("AB10"), 43792)
+    	except (ZeroDivisionError,SyntaxError,NameError,IndexError,TypeError,AssertionError,ValueError):
+        	print("ERROR => TEST FAIL from HEX_TO_DEC Function")
+        	
+    def test_oct_to_dec(self):
+    	try:
+    		self.assertEqual(calc.oct_to_dec(0), 0)
+    		self.assertEqual(calc.oct_to_dec(1), 1)
+    		self.assertEqual(calc.oct_to_dec(777777777777777), 35184372088831)
+    	except (ZeroDivisionError,SyntaxError,NameError,IndexError,TypeError,AssertionError,ValueError):
+        	print("ERROR => TEST FAIL from OCT_TO_DEC Function")
+        	
+if __name__ == '__main__':
+    unittest.main()
+    
+
